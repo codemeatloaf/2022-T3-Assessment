3860e04f5ce18d21221378be05e13361a6f8a2a9
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2022-08-12 18:38:45 +1000

    committed entire thing lol

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ciara\OneDrive\PC\Shared\Documents\GitHub\2022-T3-Assessment\.venv
+set VIRTUAL_ENV=C:\Users\Ciaran\OneDrive\PC\Shared\Documents\GitHub\2022-T3-Assessment\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # guide: https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 # imports
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def wndw1():
+    return render_template('wndw1.html')
